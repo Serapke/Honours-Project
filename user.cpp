e213a203debe821879d3702a203b08205dcde269
Mantas <M.Serapinas@gmail.com>
2018-03-05 09:34:05 +0000

    Heap allocation (0)

--- user.cpp
+++ user.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
+#include <string.h>
 
-//int c = 3;
+using namespace std;
+
+struct new_type {
+  int hello;
+  int goodbye;
+};
+
 int main() {
-//  c = 2;
-  int var;
-  int* ptr;
-  int** pptr;
-  
-  var = 3000;
-  ptr = &var;
-  pptr = &ptr;
+//  int var;
+//  int* ptr;
+//  int** pptr;
+//
+//  var = 3000;
+//  ptr = &var;
+//  pptr = &ptr;
+//
+//  printf("\nValue of var = %d\n", var);
+//  printf("\nValue available at *ptr = %d\n", *ptr);
+//  printf("\nValue available at **ptr  = %d\n", **pptr);
 
-  printf("\nValue of var = %d\n", var);
-  printf("\nValue available at *ptr = %d\n", *ptr);
-  printf("\nValue available at **ptr  = %d\n", **pptr);
-//  int* p = (int*) malloc(2*sizeof(int));
-//  printf("\n%i\n\n", *p);
-//  *p = c;
-//  int i = 4;
-//  r = p;
-//  *r = 3;
-//  *(p+1) = 2;
-//  printf("\n%i\n\n", *p);
-//  j = 4;
-//  i = j + 5;
-//  for (int i = 0; i < 20; i++) {
-//    a[i] = a[i * 4] + 5;
-//    if (i > 3)
-//      break;
-//  }
+//  Struct pointers have to be cast to void pointers for loads and stores
+//  new_type a;
+//  a.hello = 1;
+//  a.goodbye = 2;
+//  void* p_a = (void*) &a;
+//  void* p = p_a;
+
+  int* heap_ptr = (int*) malloc(2*sizeof(int));
+  cout << heap_ptr << endl;
+//  heap_ptr[0] = 1;
+//  printf("\nAddress of heap_ptr = %p", heap_ptr);
+//  printf("\nValue of heap_ptr = %d\n", heap_ptr[0]);
+//  free(heap_ptr);
+
+
+//  char str[] = "almost every programmer should know memset!";
+//  memset (str,'-',6);
+//  puts (str);
+
   return 0;
 }
+
 
